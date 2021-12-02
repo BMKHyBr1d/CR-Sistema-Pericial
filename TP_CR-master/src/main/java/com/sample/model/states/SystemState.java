baab5d350bf02567ba428e75923314b2b9158af6
Diogo Lopes <81159990+BMKHyBr1d@users.noreply.github.com>
2021-12-02 12:36:10 +0000

    Delete TP_CR-master directory

--- TP_CR-master/src/main/java/com/sample/model/states/SystemState.java
+++
@@ -1,8 +0,0 @@
-package com.sample.model.states;
-
-public interface SystemState {
-
-	public SystemState redirectToMode1();
-	public SystemState redirectToMode2();
-	public SystemState redirectToOpenningScreen();
-}
